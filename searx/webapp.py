emirebreo/SearxNG

[mod] Custom searx_version display
searx_version is replaced by searx_version_custom and displays the latest upstream commit with the latest fork commit.

searx/webapp.py
@@ -92,7 +92,7 @@ from searx.utils import (
     dict_subset,
     match_language,
 )
-from searx.version import VERSION_STRING, GIT_URL, GIT_BRANCH
+from searx.version import VERSION_STRING, VERSION_STRING_UPSTREAM, FORK_COMMIT, GIT_URL, GIT_BRANCH
 from searx.query import RawTextQuery
 from searx.plugins import Plugin, plugins, initialize as plugin_initialize
 from searx.plugins.oa_doi_rewrite import get_doi_resolver
@@ -476,6 +476,7 @@ def render(template_name: str, override_theme: str = None, **kwargs):
     kwargs['search_formats'] = [x for x in settings['search']['formats'] if x != 'html']
     kwargs['instance_name'] = get_setting('general.instance_name')
     kwargs['searx_version'] = VERSION_STRING
+    kwargs['searx_version_custom'] = str(VERSION_STRING_UPSTREAM) + " (" + str(FORK_COMMIT) + ")"
     kwargs['searx_git_url'] = GIT_URL
     kwargs['get_setting'] = get_setting
     kwargs['get_pretty_url'] = get_pretty_url
@@ -1365,7 +1366,7 @@ def config():
             'autocomplete': settings['search']['autocomplete'],
             'safe_search': settings['search']['safe_search'],
             'default_theme': settings['ui']['default_theme'],
-            'version': VERSION_STRING,
+            'version': str(VERSION_STRING_UPSTREAM) + " (" + str(FORK_COMMIT) + ")",
             'brand': {
                 'CONTACT_URL': get_setting('general.contact_url'),
                 'GIT_URL': GIT_URL,

Immortality-IMT/searxng_rebrand

searx/webapp.py
@@ -90,7 +90,7 @@ from searx.webadapter import (
     parse_lang,
 )
 from searx.utils import gen_useragent, dict_subset
-from searx.version import VERSION_STRING, GIT_URL, GIT_BRANCH
+from searx.version import VERSION_STRING, GIT_URL, GIT_BRANCH, FORK_COMMIT, VERSION_STRING_UPSTREAM
 from searx.query import RawTextQuery
 from searx.plugins.oa_doi_rewrite import get_doi_resolver
 from searx.preferences import (
@@ -407,6 +407,7 @@ def render(template_name: str, **kwargs):
     kwargs['search_formats'] = [x for x in settings['search']['formats'] if x != 'html']
     kwargs['instance_name'] = get_setting('general.instance_name')
     kwargs['searx_version'] = VERSION_STRING
+    kwargs['searx_version_custom'] = str(VERSION_STRING_UPSTREAM) + " (" + str(FORK_COMMIT) + ")"
     kwargs['searx_git_url'] = GIT_URL
     kwargs['enable_metrics'] = get_setting('general.enable_metrics')
     kwargs['get_setting'] = get_setting
@@ -1307,7 +1308,7 @@ def config():
             'autocomplete': settings['search']['autocomplete'],
             'safe_search': settings['search']['safe_search'],
             'default_theme': settings['ui']['default_theme'],
-            'version': VERSION_STRING,
+            'version': str(VERSION_STRING_UPSTREAM) + " (" + str(FORK_COMMIT) + ")",
             'brand': {
                 'PRIVACYPOLICY_URL': get_setting('general.privacypolicy_url'),
                 'CONTACT_URL': get_setting('general.contact_url'),